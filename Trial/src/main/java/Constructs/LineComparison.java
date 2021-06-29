5e35e96c3128c8a921aebffba941bbfe431028dc
sneha-chavan <snehachavan115@gmail.com>
2021-06-29 21:30:14 +0530

    UC2_LineEquality_LineComparison

--- Trial/src/main/java/Constructs/LineComparison.java
+++ Trial/src/main/java/Constructs/LineComparison.java
@@ -1,10 +1,18 @@
 package Constructs;
 
 public class LineComparison {
-	public static final int x1=2;
-	public static final int y1=3;
-	public static final int x2=5;
-	public static final int y2=6;
+	public static int x1;
+	public static int y1;
+	public static int x2;
+	public static int y2;
+	public static int lengthXY,lengthAB;
+	
+	public LineComparison(int x1,int y1, int x2, int y2) {
+		this.x1=x1;
+		this.x2=x2;
+		this.y1=y1;
+		this.y2=y2;
+	}
 	
 	public static int LengthOfLine() {
 		int length;
@@ -13,11 +21,27 @@ public class LineComparison {
 		length = (int)Math.sqrt(x+y);
 		return length;
 	}
+	public static Boolean lineEquality(Integer lineOne, Integer lineTwo) {
+		if (lineOne.equals(lineTwo)) {
+			return true;
+		}
+		return false;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Computation Program");
-		
-		System.out.println("Length of Line: "+LengthOfLine());
+		LineComparison lineXY = new LineComparison(2, 2, 5, 4);
+		Integer lengthXY = LengthOfLine();
+		LineComparison lineAB = new LineComparison(9, 4, 2, 6);
+		Integer lengthAB = LengthOfLine();
+//		System.out.println(lengthAB);
+//		System.out.println(lengthXY);
+		if (lineEquality(lengthXY, lengthAB)) {
+			System.out.println("Both lines are euqal with length: "+ lengthAB);
+		}
+		else {
+			System.out.println("Lines having different lengths");
+		}
 	}
 
 }
