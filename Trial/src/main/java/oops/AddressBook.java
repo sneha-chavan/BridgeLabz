a35e25b5639d1c896da8718c043ea006510b7b20
sneha-chavan <snehachavan115@gmail.com>
2021-06-30 01:27:43 +0530

    ability to add contact

--- Trial/src/main/java/oops/AddressBook.java
+++ Trial/src/main/java/oops/AddressBook.java
@@ -1,6 +1,23 @@
 package oops;
 
 public class AddressBook {
+	private static String firstName;
+	private static String lastName;
+	private static String address;
+	private static String city;
+	private static String state;
+	private static int zip;
+	private static int phoneNumber;
+	
+	public static void AddressBook(String fn, String ln, String add, String c, String s, int z, int pn) {
+		firstName= fn;
+		lastName = ln;
+		address = add;
+		city = c;
+		state = s;
+		zip = z;
+		phoneNumber = pn;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
