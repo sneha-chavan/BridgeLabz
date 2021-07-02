f7622a6b0384489fa33ace74853b75a8c840a88a
sneha-chavan <snehachavan115@gmail.com>
2021-07-03 00:58:56 +0530

    Address Book

--- Trial/src/main/java/oops/AddressBook.java
+++ Trial/src/main/java/oops/AddressBook.java
@@ -1,27 +1,136 @@
 package oops;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class AddressBook {
-	private static String firstName;
-	private static String lastName;
-	private static String address;
-	private static String city;
-	private static String state;
-	private static int zip;
-	private static int phoneNumber;
+	static Scanner sc = new Scanner(System.in);
+	ArrayList<Contact> contact;
 	
-	public static void AddressBook(String fn, String ln, String add, String c, String s, int z, int pn) {
-		firstName= fn;
-		lastName = ln;
-		address = add;
-		city = c;
-		state = s;
-		zip = z;
-		phoneNumber = pn;
+	public AddressBook() {
+		contact = new ArrayList<>();
+	}
+	
+	
+	
+	private void addContact(String firstName, String lastName, String address, String city, String state, int zip,
+			int phoneNumber) {
+		contact.add(new Contact(firstName,lastName,address,city,state,zip,phoneNumber));
+	}
+	
+	private void addMultipleContacts() {
+		String more;
+		do {
+			System.out.println("First Name: ");
+			String fName=sc.nextLine();
+			System.out.println("Last Name: ");
+			String lName=sc.nextLine();
+			System.out.println("Address: ");
+			String address=sc.nextLine();
+			System.out.println("City: ");
+			String city=sc.nextLine();
+			System.out.println("State: ");
+			String state=sc.nextLine();
+			System.out.println("zip: ");
+			int zip=sc.nextInt();
+			System.out.println("6 digit phone number: ");
+			int PhNo=sc.nextInt();
+			
+			addContact(fName, lName, address, city, state, zip, PhNo);
+			
+			System.out.println("add one more? Y or N: ");
+			more=sc.nextLine();
+			if (more == "NO" || more == "N") {
+				break;
+			}
+		}while(more == "Y" || more == "YES");
+		
+	}
+	
+	private void displayContacts() {
+		for (int i = 0; i < contact.size(); i++) {
+			
+			System.out.println(i+1+")\t"+ contact.get(i).getFirstName()+"\t"+contact.get(i).getLastName()+"\t"+
+					contact.get(i).getAddress()+"\t"+contact.get(i).getCity()+"\t"+contact.get(i).getState()+"\t"+
+					contact.get(i).getZip()+"\t"+contact.get(i).getPhoneNumber());		}
+		
+	}
+	
+		
+	private void deleteContact() {
+		System.out.println("Enter your First name:");
+		String fname = sc.next();
+		
+		Iterator<Contact> iterator = contact.listIterator();
+		
+		while (iterator.hasNext()) {
+			Contact person = iterator.next();
+			if (fname.equals(person.getFirstName())) {
+				contact.remove(person);
+			}
+		}
+	}
+	
+	private void editContact() {
+		System.out.println("Enter your First name:");
+		String fname = sc.next();
+
+		Iterator<Contact> iterator = contact.listIterator();
+		while (iterator.hasNext()) {
+			Contact person = iterator.next();
+
+			if (fname.equals(person.getFirstName())) {
+				System.out.println("Choose field you want to add:");
+				System.out.println("1.Last Name\t2.Phone Number ");
+				switch (sc.nextInt()) {
+				case 1:
+					System.out.println("Re-Correct your lastname");
+					person.setLastName(sc.next());
+					break;
+				case 2:
+					System.out.println("Re-Correct your Phone Number");
+					person.setPhoneNumber(sc.nextInt());
+					break;
+				}
+			}
+		}	
+		displayContact(fname);
+	}
+	
+	private void displayContact(String name) {
+		Iterator<Contact> iterator = contact.listIterator();
+		while (iterator.hasNext()) {
+			Contact person = iterator.next();
+
+			if (name.equals(person.getFirstName())) {
+				System.out.println("updated contact ");
+				System.out.println(person.getFirstName()+"\t"+person.getLastName()+"\t"+person.getPhoneNumber()+"\t"+person.getCity());
+			}
+		}
 	}
 
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Address Book Program with Object Oriented Concept");
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact("Sneha", "Chavan", "Ghatkopar", "Mumbai", "Maharashtra", 400084, 986767);
+		addressBook.addContact("Jagruti", "Khichi", "Lalbaugh", "Mumbai", "Maharashtra", 400001, 8451829);
+		addressBook.displayContacts();
+		System.out.println("Choose field you want to do:");
+		System.out.println("1.Edit \t2.Delete \t3.Add Multiple Contacts \t4.Exit ");
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1:
+			addressBook.editContact();
+			break;
+		case 2:
+			addressBook.deleteContact();
+		case 3:
+			addressBook.addMultipleContacts();
+		default:
+			break;
+		}
+		
 	}
 
 }
---
+++ Trial/src/main/java/oops/Contact.java
@@ -0,0 +1,93 @@
+package oops;
+
+public class Contact {
+
+	private  String firstName;
+	private  String lastName;
+	private  String address;
+	private  String city;
+	private  String state;
+	private   int zip;
+	private  int phoneNumber;
+	
+	
+	public Contact() {
+		super();
+	}
+
+	public Contact(String firstName, String lastName, String address, String city, String state, int zip,
+			int phoneNumber) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + "]";
+	}
+	
+	
+}
