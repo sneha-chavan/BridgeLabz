49bea9e77d4fd210a8878b11efbd2a383cfc1ec8
sneha-chavan <snehachavan115@gmail.com>
2021-07-04 01:35:40 +0530

    UC2_Generics_Common Generics for all threee different DataTypes

--- Trial/src/test/java/Generic/Max/MaxIntTest.java
+++ Trial/src/test/java/Generic/Max/MaxIntTest.java
@@ -1,6 +1,6 @@
 package Generic.Max;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,23 +8,77 @@ import org.junit.Test;
 import junit.framework.Assert;
 
 public class MaxIntTest {
-	MaxInt maxInt;
-	Integer x=null;
-	Integer y= null;
-	Integer z = null;
+	MaxInt<Integer> maxInt;
+	Integer x;
+	Integer y;
+	Integer z;
+	String a="girls";
+	String s="boys";
+	String d="defaulter";
 	
+
 	@Before
 	public void setUp() throws Exception {
-		maxInt = new MaxInt(x, y, z);
+		maxInt = new MaxInt<Integer>(x, y, z);
 	}
-
+	
+	//Testing for Integer type data in common generics
+	
 	@SuppressWarnings({ "static-access", "deprecation" })
 	@Test
-	public void testFirstElementMax() {
+	public void testFirstIntegerElementMax() {
 		x=9;
 		y=3;
 		z=4;
-		Assert.assertEquals(x, maxInt.maxInt(x,y,z));
+		Assert.assertEquals(x, maxInt.maximum(x,y,z));
 	}
+	
+	@SuppressWarnings({ "static-access", "deprecation" })
+	@Test
+	public void testSecondIntegerElementMax() {
+		x=9;
+		y=10;
+		z=4;
+		Assert.assertEquals(y, maxInt.maximum(x,y,z));
+	}
+	
+	@SuppressWarnings({ "static-access", "deprecation" })
+	@Test
+	public void testThirdIntegerElementMax() {
+		x=9;
+		y=0;
+		z=10;
+		Assert.assertEquals(z, maxInt.maximum(x,y,z));
+	}
+
+	//For testing String Type Data in common Generic 
+	
+	@SuppressWarnings({ "static-access", "deprecation" })
+	@Test
+	public void testFirstStringElementMax() {
+		a="FARRARI";
+		s="BUGATI";
+		d="AUDI";
+		Assert.assertEquals(a, maxInt.maximum(a,s,d));
+	}
+	
+	@SuppressWarnings({ "static-access", "deprecation" })
+	@Test
+	public void testSecondStringElementMax() {
+		a="LTI";
+		s="TATA";
+		d="BridgeLabz";
+		Assert.assertEquals(s, maxInt.maximum(a,s,d));
+	}
+	
+	@SuppressWarnings({ "static-access", "deprecation" })
+	@Test
+	public void testThirdStringElementMax() {
+		a="Appendable";
+		s="EnumConstantNotPresentException";
+		d="NumberFormatException";
+		Assert.assertEquals(d, maxInt.maximum(a,s,d));
+	}
+
 
 }
